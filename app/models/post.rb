fb32766da124c281413bf05f20b43bc689a53300
Jonathan Chan <johnkchan252@gmail.com>
2019-02-03 05:52:57 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :size, inclusion: { in: %w(small medium large)}
+  validates :size, inclusion: { in: %w(Fiction Non-Fiction)}
   validates :content, length: { minimum: 100 }
 end
